6d8f2df9da3697873bb3ac76ada77ad27efa66fd
ESO-Esports-online <165462845+ESO-Esports-online@users.noreply.github.com>
2024-04-04 11:12:46 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2024/06/01';
+        var finalDate = '2024/04/04';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
